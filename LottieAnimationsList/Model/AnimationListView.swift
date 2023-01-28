c2abcb5f5b64a330918dc4081ca60c28c2dce92a
Tom <twu@lcs.on.ca>
2023-01-28 10:08:49 -0500

    Added the third-party lottie-ios library.

---
+++ LottieAnimationsList/Model/AnimationListView.swift
@@ -0,0 +1,20 @@
+//
+//  AnimationListView.swift
+//  LottieAnimationsList
+//
+//  Created by Tom Wu on 2023-01-28.
+//
+
+import SwiftUI
+
+struct AnimationListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AnimationListView_Previews: PreviewProvider {
+    static var previews: some View {
+        AnimationListView()
+    }
+}
